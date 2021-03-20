a24f938602b69fb0037430af00e37c6efe1567d6
samnang.hen <hensamnang.sn@gmail.com>
2021-03-20 11:58:00 +0700

    menu

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+@section('main')
+    <ul>
+        <li><a href="#"></a></li>
+        <li><a href="">HOME</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">CATOLOG</a></li>
+        <li><a href="">PAGES</a></li>
+        <li><a href="">SERVICES</a></li>
+        <li><a href="">CONTACTS</a></li>
+        <li><a href="">BLOCKS</a></li>
+    </ul>
+@endsection
